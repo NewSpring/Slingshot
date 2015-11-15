875e833c34e28803aa703f12c2d1e52bce676995
James Baxley <james.baxley@newspring.cc>
2015-11-15 12:27:01 -0500

    reworked plugins to allow norma and meteor to run side by side

--- entry/client/webpack.conf.js
+++ entry/client/webpack.conf.js
@@ -10,9 +10,6 @@ if (process.env.NODE_ENV !== 'production') {
         transform: 'react-transform-hmr',
         imports: ['react'],
         locals: ['module']
-      }, {
-        transform: 'react-transform-catch-errors',
-        imports: ['react', 'redbox-react']
       }]
       // redbox-react is breaking the line numbers :-(
       // you might want to disable it
@@ -24,7 +21,12 @@ module.exports = {
   entry: './entry',
   module: {
     loaders: [
-      { test: /\.jsx?$/, loader: 'babel', query: babelSettings, exclude: /node_modules/ },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        query: babelSettings,
+        exclude: /node_modules/
+      },
       { test: /\.css$/, loader: 'style!css' },
       { test: /\.(png|jpe?g)(\?.*)?$/, loader: 'url?limit=8182' },
       { test: /\.(svg|ttf|woff|eot)(\?.*)?$/, loader: 'file' }
