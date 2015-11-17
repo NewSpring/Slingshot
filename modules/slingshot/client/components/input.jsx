15b04c0d9bf5a1df09d742f949c1ce05aad22d86
James Baxley <james.baxley@newspring.cc>
2015-11-17 16:02:22 -0500

    working validation check

--- modules/slingshot/client/components/input.jsx
+++ modules/slingshot/client/components/input.jsx
@@ -1,41 +1,32 @@
-import { Component } from "react";
+import React from "react";
 
-export default class Input extends Component {
 
-  constructor(props) {
+const Input = React.createClass({
 
-    super(props);
+  componentWillMount() {
 
-    var labelFor;
-    var labelName;
-    var Placeholder;
-    var className;
-    var tabIndex;
-    var regex;
-    var userAgent;
-
-    if (props.id) {
-      this.labelFor = props.id;
-    } else if (props.name) {
-      this.labelFor = props.name;
-    } else if (props.label) {
-      this.labelFor = props.label;
+    if (this.props.id) {
+      this.labelFor = this.props.id;
+    } else if (this.props.name) {
+      this.labelFor = this.props.name;
+    } else if (this.props.label) {
+      this.labelFor = this.props.label;
     }
 
-    if (props.label) {
-      this.labelName = props.label;
-    } else if (props.name) {
-      this.labelName = props.name;
+    if (this.props.label) {
+      this.labelName = this.props.label;
+    } else if (this.props.name) {
+      this.labelName = this.props.name;
     }
 
-    if (props.placeholder) {
-      this.Placeholder = props.placeholder;
-    } else if (props.label) {
-      this.Placeholder = props.label;
+    if (this.props.placeholder) {
+      this.Placeholder = this.props.placeholder;
+    } else if (this.props.label) {
+      this.Placeholder = this.props.label;
     }
 
-    var regex = /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i
-    var userAgent = navigator.userAgent;
+    const regex = /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i
+    const userAgent = navigator.userAgent;
 
     if (regex.test(userAgent)) {
       this.tabIndex = -1;
@@ -43,45 +34,34 @@ export default class Input extends Component {
       this.tabIndex = 0;
     }
 
-  }
+  },
 
-  doThings(){
-
-  }
-
-  validate(event) {
+  validate: function validate() {
+    const value = event.target.value;
     if (this.props.validation && typeof(this.props.validation) === "function") {
-      const value = event.target.value;
+
       this.replaceState({
         validating: this.props.validation(value)
       });
     }
-  }
+  },
 
   disabled() {
     if (this.props.disabled) {
       return disabled;
     }
-  }
+  },
 
   renderHelpText(message) {
-    if (this.state.validating) {
+    if (this.state && !this.state.validating) {
       return (
         <span className="input__status">
-          {message}
+          {this.props.validationErrorMessage}
         </span>
       );
-    } else {
-      this.setState({validating: true});
     }
 
-  }
-
-  componentDidMount() {
-    this.setState({
-      validating: true
-    });
-  }
+  },
 
   render() {
 
@@ -109,4 +89,7 @@ export default class Input extends Component {
 
   }
 
-}
+})
+
+
+export default Input
