ebfb06b741eaed166aae15f05adb00a246ae13a1
edolyne <ed@edolyne.com>
2015-11-17 15:32:56 -0500

    Work on set state to prepare for validation

--- modules/slingshot/client/home.jsx
+++ modules/slingshot/client/home.jsx
@@ -5,6 +5,7 @@ import NavBar from "./sections/navbar"
 import Hero from "./sections/hero"
 import Ad from "./sections/ad"
 import Footer from "./sections/footer"
+import Input from "./components/input"
 
 // TODO: import collections
 
@@ -28,6 +29,21 @@ const Home = React.createClass({
         <NavBar />
         <Hero />
         <Ad />
+
+        <form>
+          <Input
+            label="Layout Label"
+            name="firstName"
+            type="text"
+            placeholder="Test Placeholder"
+            class="stuff"
+            disabled=""
+            id="firstName"
+            validation={this.isValid}
+            validationErrorMessage="You Need A Name"
+          />
+        </form>
+
         <Footer />
       </div>
     );
