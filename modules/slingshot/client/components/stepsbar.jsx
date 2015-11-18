6f0a5116881571ae5cc8b3c01098c63a1e76f40c
briankalwat <brian@briankalwat.com>
2015-11-18 18:02:30 -0500

    moved steps component styling to complex object

--- modules/slingshot/client/components/stepsbar.jsx
+++ modules/slingshot/client/components/stepsbar.jsx
@@ -5,10 +5,10 @@ const StepsBar = React.createClass({
   render() {
 
     return (
-      <ul className="list-steps">
-      	<li className="step--completed">1</li>
-		<li className="step--active">2</li>
-      	<li>3</li>
+      <ul className="steps">
+      	<li className="steps__item steps__item--completed">1</li>
+		<li className="steps__item steps__item--active">2</li>
+      	<li className="steps__item">3</li>
       </ul>
     );
 
