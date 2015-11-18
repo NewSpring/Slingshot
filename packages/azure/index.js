c87fe7c5fd5ba983d980a0a8300c427134382a75
John Pinkerton <jpinkerton88@gmail.com>
2015-11-18 12:40:22 -0500

    add methods for creating and deleting resource groups

--- packages/azure/index.js
+++ packages/azure/index.js
@@ -2,6 +2,7 @@
 const AzureCommon = Npm.require("azure-common");
 const Dns = Npm.require("azure-arm-dns");
 const Adal = Npm.require("adal-node");
+const Resource = Npm.require("azure-arm-resource");
 
 
 Azure = {};
@@ -164,4 +165,81 @@ Azure.cname.exists = (dnsClient, cname, cb) => {
     cb(exists);
 
   });
+}
+
+Azure.resourceGroup = {}
+Azure.resourceGroup.create = (name) => {
+  check(name, String);
+
+  Azure.token.get((err, result) => {
+
+    if (err) {
+      throw new Meteor.Error(`Unable to authenticate: ${err.stack}`);
+    }
+
+    let credentials = new AzureCommon.TokenCloudCredentials({
+      subscriptionId: Meteor.settings.azure.AZURE_SUBSCRIPTION_ID,
+      token: result.accessToken
+    });
+
+    let resourceClient = Resource.createResourceManagementClient(credentials, resourceURI);
+
+    resourceClient.resourceGroups.checkExistence(name, (err, result) => {
+
+      if (err.code === "NotFound") {
+        const parameters = {
+          location: "East US"
+        }
+
+        resourceClient.resourceGroups.createOrUpdate(
+          name,
+          parameters,
+          (err, result) => {
+
+          if (err) {
+            throw new Meteor.Error(`Could not create resource group: ${err.stack}`);
+          }
+
+          console.log(result);
+
+        });
+      }
+
+      else {
+        throw new Meteor.Error(`Resource Group ${name} already exists`);
+      }
+
+    });
+
+  });
+
+}
+
+Azure.resourceGroup.remove = (name) => {
+  check(name, String);
+
+  Azure.token.get((err, result) => {
+
+    if (err) {
+      throw new Meteor.Error(`Unable to authenticate: ${err.stack}`);
+    }
+
+    let credentials = new AzureCommon.TokenCloudCredentials({
+      subscriptionId: Meteor.settings.azure.AZURE_SUBSCRIPTION_ID,
+      token: result.accessToken
+    });
+
+    let resourceClient = Resource.createResourceManagementClient(credentials, resourceURI);
+
+    resourceClient.resourceGroups.deleteMethod(name, (err, result) => {
+
+      if (err) {
+        throw new Meteor.Error(`Unable to delete resource group ${name}: ${err.stack}`);
+      }
+
+      console.log(result);
+    });
+
+  });
+
 }
