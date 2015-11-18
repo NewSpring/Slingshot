96a69e29dee06c696f05f38ff29a90e221ce0869
John Pinkerton <jpinkerton88@gmail.com>
2015-11-18 18:31:14 -0500

    make steps dynamic

--- modules/slingshot/client/components/stepsbar.jsx
+++ modules/slingshot/client/components/stepsbar.jsx
@@ -4,11 +4,22 @@ const StepsBar = React.createClass({
 
   render() {
 
+    let steps = [];
+    for (let i = 1; i <= this.props.steps; i++) {
+      if (i < this.props.active) {
+        steps.push(<li className="steps__item steps__item--completed">{i}</li>);
+      }
+      else if (i === this.props.active) {
+        steps.push(<li className="steps__item steps__item--active">{i}</li>);
+      }
+      else {
+        steps.push(<li className="steps__item">{i}</li>);
+      }
+    }
+
     return (
       <ul className="steps">
-      	<li className="steps__item steps__item--completed">1</li>
-		<li className="steps__item steps__item--active">2</li>
-      	<li className="steps__item">3</li>
+        {steps}
       </ul>
     );
 
