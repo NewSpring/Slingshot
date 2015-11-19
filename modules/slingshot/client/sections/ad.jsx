f354d20040d28cd5e594ebaa0064b5b4c3ce8168
ben.wiley <ben.wiley@newspring.cc>
2015-11-19 10:39:37 -0500

    Merge new HTML from master with ads pulled from Rock as JSON

--- modules/slingshot/client/sections/ad.jsx
+++ modules/slingshot/client/sections/ad.jsx
@@ -1,24 +1,31 @@
 import React from "react";
+import { Link } from 'react-router'
 
 const Ad = React.createClass({
 
   render() {
     return (
-      <div className="grid__item hard one-whole@handheld one-half@lap-and-up">
-        <div className="soft soft-double-ends">
-          <div>
-            <h2 className="text-primary">{ this.props.title }</h2>
-            <h4>{ this.props.subTitle }</h4>
-            <div className="soft">
-              <ul>
-                { this.props.points.map(function (p, index) {
-                  return (
-                    <li key={index}>{ p }</li>
-                  );
-                }) }
-              </ul>
-            </div>
-            <button className="btn--filled">Get Started</button>
+      <div className="grid__item one-whole@handheld one-half@lap-and-up">
+        <div className="soft-double-ends soft-double-sides@lap-and-up">
+          <div className="push-double-sides@lap-and-up">
+            <h2 className="text-primary text-center@handheld">
+              { this.props.title }
+            </h2>
+            <h4>
+              { this.props.subTitle }
+            </h4>
+            <ul>
+              { this.props.points.map(function (point, index) {
+                return (
+                  <li key={index}>{ point }</li>
+                );
+              }) }
+            </ul>
+            <Link to={ this.props.link }>
+              <button className="btn--filled one-whole@handheld push-ends">
+                { this.props.linkText }
+              </button>
+            </Link>
           </div>
         </div>
       </div>
