73c03c9888a9920deb8df75b5071002ca230b387
samclaridge <st.claridge@gmail.com>
2015-11-19 17:05:34 -0500

    update plans ui

--- modules/slingshot/client/components/plan.jsx
+++ modules/slingshot/client/components/plan.jsx
@@ -11,6 +11,7 @@ const Plan = React.createClass({
     this.props.save(this.refs.plan.value)
   },
 
+
   render() {
 
     let outlinedClasses = [
@@ -28,16 +29,35 @@ const Plan = React.createClass({
       outlinedClasses.push("push-top");
     }
 
-    const buttonClasses = this.props.plan.featured ? "btn--filled" : "btn--secondary";
+    let buttonClasses = this.props.plan.featured ? "btn--filled" : "btn--secondary";
+    buttonClasses += " push-half-top";
+
+
     return (
       <div className="grid__item one-third@lap-and-up push-bottom">
         <div className={outlinedClasses.join(" ")}>
           <div className="soft-ends">
+
+            {(() => {
+              if (this.props.plan.featured) {
+                return (
+                  <div className="triangle">MOST POPULAR</div>
+                )
+              }
+
+            })()}
+
             <input ref="plan" type="hidden" value={this.props.plan.id}/>
             <img src={this.props.plan.image} height="100" className="push-bottom" />
             <h4 className="text-dark-tertiary">{this.props.plan.name}</h4>
-            <h3 className="soft flush">${this.props.plan.price}/mo</h3>
-            <p><small>{this.props.plan.people} people</small></p>
+            <div className="grid flush">
+              <div className="display-inline-block flush soft-left" style={ {position: "relative" }}>
+                <h2>{this.props.plan.price}</h2>
+                <span className="locked-top locked-left"><h4>$</h4></span>
+              </div>
+              <h6 className="display-inline-block">/mo</h6>
+            </div>
+              <p className="push-half-bottom"><small>{this.props.plan.people} people</small></p>
             <p><small>Server with {this.props.plan.ram} RAM</small></p>
 
             <button className={buttonClasses} onClick={this.saveData}>
